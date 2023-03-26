65e1e145202a2829c28b7e2b25763917b6585306
absenth <1115138+absenth@users.noreply.github.com>
2023-03-26 07:59:59 -0500

    removing devbox which breaks stuff in odd ways

--- pkg/twitchbot.py
+++ pkg/twitchbot.py
@@ -4,15 +4,30 @@ This will share a commands.csv with the Discord bot.
 This should have CRUD operations for the commands.csv
 '''
 
+import os
 import pandas as pd
 from dotenv import load_dotenv
 from twitchio.ext import commands
 
+load_dotenv()
+twitchtoken = os.getenv('TWITCH_OAUTH_TOKEN')
 
-def main():
-  load_dotenv()
-  token = os.getenv('TWITCH_OAUTH_TOKEN')
-  pass
+class Bot(commands.Bot):
 
-if __name__ == '__main__':
-  main()
+    def __init__(self):
+        super().__init__(token=twitchtoken, client_id='client_id', nick='nick', prefix='!',
+                         initial_channels=['absenth762'])
+
+    async def event_ready(self):
+        print(f'Ready | {self.nick}')
+
+    async def event_message(self, message):
+        print(message.content)
+        await self.handle_commands(message)
+
+    @commands.command(name='hello')
+    async def my_command(self, ctx):
+        await ctx.send(f'Hello {ctx.author.name}!')
+
+bot = Bot()
+bot.run()
