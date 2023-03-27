fb295ef091d4e26f68d74257121bf90023823e89
absenth <1115138+absenth@users.noreply.github.com>
2023-03-27 08:54:23 -0500

    starting to stub out the CSV reader

--- pkg/twitchbot.py
+++ pkg/twitchbot.py
@@ -4,6 +4,7 @@ This will share a commands.csv with the Discord bot.
 This should have CRUD operations for the commands.csv
 '''
 
+import csv
 import os
 import pandas as pd
 from dotenv import load_dotenv
@@ -13,21 +14,26 @@ load_dotenv()
 twitchtoken = os.getenv('TWITCH_OAUTH_TOKEN')
 
 class Bot(commands.Bot):
-
     def __init__(self):
         super().__init__(token=twitchtoken, client_id='client_id', nick='nick', prefix='!',
                          initial_channels=['absenth762'])
+        self.load_commands()
 
-    async def event_ready(self):
-        print(f'Ready | {self.nick}')
+    def load_commands(self):
+        with open('commands.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                command_name = row['Commands']
+                command_output = row['Output']
+                command_func = self.make_command_func(command_output)
+                self.add_command(command_name, command_func)
 
-    async def event_message(self, message):
-        print(message.content)
-        await self.handle_commands(message)
-
-    @commands.command(name='hello')
-    async def my_command(self, ctx):
-        await ctx.send(f'Hello {ctx.author.name}!')
+    def make_command_func(self, output):
+        async def command_func(self, ctx):
+            message = output.replace('{message.channel}', ctx.channel.name)
+            message = message.replace('{message.text_args[0]}', ctx.content.split()[1])
+            await ctx.send(message)
+        return command_func
 
 bot = Bot()
 bot.run()
