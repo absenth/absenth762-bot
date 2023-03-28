6ea6b07514cfbc007a6244e2469f4b94673474d0
absenth <1115138+absenth@users.noreply.github.com>
2023-03-28 12:45:08 -0500

    Updating some more

--- pkg/discordbot.py
+++ pkg/discordbot.py
@@ -6,7 +6,6 @@ This should have CRUD operations for the commands.csv
 
 import discord
 import os
-import pandas as pd
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -14,15 +13,24 @@ from dotenv import load_dotenv
 load_dotenv()
 discordtoken = os.getenv('DISCORD_TOKEN')
 
-class DiscordClient(discord.Client):
-  async def on_ready(self):
-    print(f'Discord Ready |  {self.user}')
-    print(f'The Discord Bot is Online')
 
-  async def on_message(self, message):
-    # Don't respond to ourselves, that's dumb.
-    if message.author == self.user:
-      return
+class DiscordClient(discord.Client):
+    async def on_ready(self):
+        print(f'Discord Ready |  {self.user}')
+        print('The Discord Bot is Online')
+
+    async def on_message(self, message):
+        # Don't respond to ourselves, that's dumb.
+        if message.author == self.user:
+            return
+
+    def list_commands(self):
+        # Open commands.csv and create a command for each command and response
+        commands_list = []
+        if os.path.isfile("commands.csv"):
+            with open("commands.csv", "r") as f:
+                for line in f:
+                    commands_list.append(line)
 
 
 intents = discord.Intents.default()
--- pkg/twitchbot.py
+++ pkg/twitchbot.py
@@ -6,23 +6,38 @@ This should have CRUD operations for the commands.csv
 
 import csv
 import os
-import pandas as pd
 from dotenv import load_dotenv
 from twitchio.ext import commands
 
 load_dotenv()
 twitchtoken = os.getenv('TWITCH_OAUTH_TOKEN')
 
+
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(token=twitchtoken, client_id='client_id', nick='nick', prefix='!',
-                         initial_channels=['absenth762'])
+        super().__init__(token=twitchtoken, client_id='client_id', nick='nick',
+                         prefix='!', initial_channels=['absenth762'])
 
     async def event_ready(self):
         print(f'Twitch Ready | {self.nick}')
-        print(f'The Twitch Bot is Online')
+        print('The Twitch Bot is Online')
+        self.load_commands()  # load commands from the CSV file when the bot is ready
 
+    def load_commands(self):
+        # Open commands.csv and create a command for each command and response
+        if os.path.isfile("commands.csv"):
+            with open("commands.csv", "r") as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    self.commands[row['Commands']] = row['Output']
+                    setattr(self, row['Commands'].lstrip('!'), self.handle_command)
+                    print(f"Added command {row['Commands']} with output {row['Output']}")
 
+    async def handle_command(self, message):
+        # Check if the message is a command and if it is, send the appropriate response
+        if message.content in self.commands:
+            response = self.commands[message.content].format(message=message)
+            await message.channel.send(response)
 
 
 bot = Bot()
